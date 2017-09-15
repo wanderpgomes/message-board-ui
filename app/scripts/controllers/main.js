74206611dedca593892bff814b0c3378ca76f40a
wander <wander.gomes@gmail.com>
2017-09-14 22:22:54 -0400

    Initial commit. Frontend application created with yeoman angular generator.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name messageBoardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the messageBoardApp
+ */
+angular.module('messageBoardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
