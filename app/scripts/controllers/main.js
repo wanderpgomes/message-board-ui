b72121adaee859bd984471bb967f74e34b47a241
wander <wander.gomes@gmail.com>
2017-09-14 23:43:11 -0400

    Add $http call to send text to backend and display response.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the messageBoardApp
  */
 angular.module('messageBoardApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+      $scope.text = '';
+      $scope.messages = [];
+
+      $scope.send = function(){
+          if ($scope.text){
+              $http.post('http://localhost:8080/messages', {text: $scope.text})
+              .then(function success(response) {
+                  var msg = response.data.text;
+                  $scope.messages.push(msg);
+              }, function error(response) {
+
+              });
+          }
+      };
   });
